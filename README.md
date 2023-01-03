c92c1e8f902fffeceac3ac52e78afcfa67f645c1
nimids <80544703+nimids@users.noreply.github.com>
2023-01-02 23:13:17 -0800

    Update for 1.19.3
    Addressed Mojang's Registry changes.
    
    Added icon.png for Mod Menu.
    
    Updated Gradle.
    
    Dropped unneeded initialization class.
    
    Changed versioning to be {mod version}+{minecraft version} so 0.0.2+1.19.3 rather then just 0.0.2
    
    Added Mangrove Swamps as a biome for growing swamp trees.
    
    Removed requirement for Fabric API as it wasn't used. Only the Fabric loader is needed.

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Replant Swamp Trees - Minecraft Mod
 
-Planting oak saplings in swamps generate swamp trees.  The only way swamp trees appear in minecraft is by natural generation in swamps.  This mod allows the player to regrow these trees.  If you take an oak saplings (the same kind that would drop from the swamp tree) and plant it within a swamp, it will spawn a swamp tree.  If you grow the oak sapling outside of a swamp biome, you still get the expected behavior of an oak tree. 
+Planting oak saplings in swamps generate swamp trees.  The only way swamp trees appear in minecraft is by natural generation in swamps.  This mod allows the player to regrow these trees.  If you take an oak saplings (the same kind that would drop from the swamp tree) and plant it within a swamp, it will spawn a swamp tree.  If you grow the oak sapling outside a swamp biome, you still get the expected behavior of an oak tree. 
 
-Requires Fabric.  (contact me if you think I can do this without Fabric, I'm new to modding.) 
+Requires Fabric Loader, but does not need Fabric API.  
+As the mod is a simple mixin it should be easy to modify it to work with another loader, as long as it supports sponge mixins.
 
 Works on both client and server.
 
--- src/main/java/net/rutrum/replantswamp/mixin/SwampFromSaplings.java
+++ src/main/java/net/rutrum/replantswamp/mixin/SwampFromSaplings.java
@@ -1,5 +1,9 @@
 package net.rutrum.replantswamp.mixin;
 
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -9,9 +13,6 @@ import net.minecraft.block.sapling.SaplingGenerator;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryEntry;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
@@ -23,31 +24,31 @@ public class SwampFromSaplings {
 		method = "generate",
 		at = @At(
 			value = "INVOKE_ASSIGN",
-			target = "Lnet/minecraft/block/sapling/SaplingGenerator;getTreeFeature"
-				+ "(Lnet/minecraft/util/math/random/Random;Z)"
-				+ "Lnet/minecraft/util/registry/RegistryEntry;"
+			target = "Lnet/minecraft/block/sapling/SaplingGenerator;getTreeFeature(Lnet/minecraft/util/math/random/Random;Z)Lnet/minecraft/registry/RegistryKey;"
 		)
 	)
-	private RegistryEntry<? extends ConfiguredFeature<?, ?>> mask_oak_trees(
-		RegistryEntry<? extends ConfiguredFeature<?, ?>> tree,
-		ServerWorld world, ChunkGenerator chunkGenerator,
-		BlockPos pos, BlockState state, Random random
-	) {
-		if (tree == TreeConfiguredFeatures.OAK
-				|| tree == TreeConfiguredFeatures.OAK_BEES_0002
-				|| tree == TreeConfiguredFeatures.OAK_BEES_002
-				|| tree == TreeConfiguredFeatures.OAK_BEES_005
-				|| tree == TreeConfiguredFeatures.FANCY_OAK
-				|| tree == TreeConfiguredFeatures.FANCY_OAK_BEES_0002
-				|| tree == TreeConfiguredFeatures.FANCY_OAK_BEES_002
-				|| tree == TreeConfiguredFeatures.FANCY_OAK_BEES_005) {
-
-			Biome swamp = world.getRegistryManager().get(Registry.BIOME_KEY).entryOf(BiomeKeys.SWAMP).value();
-			if (world.getBiome(pos).value() == swamp) {
+	private RegistryKey<? extends ConfiguredFeature<?, ?>> mask_oak_trees(
+			RegistryKey<? extends ConfiguredFeature<?, ?>> tree,
+			ServerWorld world, ChunkGenerator chunkGenerator, BlockPos pos, BlockState state, Random random
+	){
+		if(
+			tree == TreeConfiguredFeatures.OAK ||
+			tree == TreeConfiguredFeatures.OAK_BEES_0002 ||
+			tree == TreeConfiguredFeatures.OAK_BEES_002 ||
+			tree == TreeConfiguredFeatures.OAK_BEES_005 ||
+			tree == TreeConfiguredFeatures.FANCY_OAK ||
+			tree == TreeConfiguredFeatures.FANCY_OAK_BEES_0002 ||
+			tree == TreeConfiguredFeatures.FANCY_OAK_BEES_002 ||
+			tree == TreeConfiguredFeatures.FANCY_OAK_BEES_005
+		){
+			Biome biome = world.getBiome(pos).value();
+			Registry<Biome> biomes = world.getRegistryManager().get(RegistryKeys.BIOME);
+			Biome swamp = biomes.entryOf(BiomeKeys.SWAMP).value();
+			Biome mangroveSwamp = biomes.entryOf(BiomeKeys.MANGROVE_SWAMP).value();
+			if(biome == swamp || biome == mangroveSwamp){
 				return TreeConfiguredFeatures.SWAMP_OAK;
 			}
 		}
 		return tree;
 	}
-
 }
