8fd30da440e62f92b5861fedc9069df031ae7986
rutrum <dave@rutrum.net>
2026-03-07 18:56:50 -0500

    First pass at 1.21.11

--- src/main/java/net/rutrum/replantswamp/mixin/SwampFromSaplings.java
+++ src/main/java/net/rutrum/replantswamp/mixin/SwampFromSaplings.java
@@ -1,54 +1,48 @@
 package net.rutrum.replantswamp.mixin;
 
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.world.biome.Biome;
+import net.minecraft.core.BlockPos;
+import net.minecraft.data.worldgen.features.TreeFeatures;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.block.grower.TreeGrower;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.sapling.SaplingGenerator;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.random.Random;
-import net.minecraft.world.biome.BiomeKeys;
-import net.minecraft.world.gen.chunk.ChunkGenerator;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.TreeConfiguredFeatures;
-
-@Mixin(SaplingGenerator.class)
+@Mixin(TreeGrower.class)
 public class SwampFromSaplings {
-	@ModifyVariable(
-		method = "generate",
-		at = @At(
-			value = "INVOKE_ASSIGN",
-			target = "Lnet/minecraft/block/sapling/SaplingGenerator;getTreeFeature(Lnet/minecraft/util/math/random/Random;Z)Lnet/minecraft/registry/RegistryKey;"
-		)
-	)
-	private RegistryKey<? extends ConfiguredFeature<?, ?>> mask_oak_trees(
-			RegistryKey<? extends ConfiguredFeature<?, ?>> tree,
-			ServerWorld world, ChunkGenerator chunkGenerator, BlockPos pos, BlockState state, Random random
-	){
-		if(
-			tree == TreeConfiguredFeatures.OAK ||
-			tree == TreeConfiguredFeatures.OAK_BEES_0002 ||
-			tree == TreeConfiguredFeatures.OAK_BEES_002 ||
-			tree == TreeConfiguredFeatures.OAK_BEES_005 ||
-			tree == TreeConfiguredFeatures.FANCY_OAK ||
-			tree == TreeConfiguredFeatures.FANCY_OAK_BEES_0002 ||
-			tree == TreeConfiguredFeatures.FANCY_OAK_BEES_002 ||
-			tree == TreeConfiguredFeatures.FANCY_OAK_BEES_005
-		){
-			Biome biome = world.getBiome(pos).value();
-			Registry<Biome> biomes = world.getRegistryManager().get(RegistryKeys.BIOME);
-			Biome swamp = biomes.entryOf(BiomeKeys.SWAMP).value();
-			Biome mangroveSwamp = biomes.entryOf(BiomeKeys.MANGROVE_SWAMP).value();
-			if(biome == swamp || biome == mangroveSwamp){
-				return TreeConfiguredFeatures.SWAMP_OAK;
-			}
-		}
-		return tree;
-	}
+    @ModifyVariable(
+        method = "growTree",
+        at = @At(
+            value = "INVOKE_ASSIGN",
+            target = "Lnet/minecraft/world/level/block/grower/TreeGrower;getConfiguredFeature(Lnet/minecraft/util/RandomSource;Z)Lnet/minecraft/resources/ResourceKey;"
+        )
+    )
+    private ResourceKey<ConfiguredFeature<?, ?>> replaceOakWithSwampOak(
+            ResourceKey<ConfiguredFeature<?, ?>> tree,
+            ServerLevel level, ChunkGenerator chunkGenerator, BlockPos pos, BlockState state, RandomSource random
+    ) {
+        if (isOakVariant(tree)) {
+            var biomeKey = level.getBiome(pos).unwrapKey().orElse(null);
+            if (biomeKey == Biomes.SWAMP || biomeKey == Biomes.MANGROVE_SWAMP) {
+                return TreeFeatures.SWAMP_OAK;
+            }
+        }
+        return tree;
+    }
+
+    private static boolean isOakVariant(ResourceKey<ConfiguredFeature<?, ?>> tree) {
+        return tree == TreeFeatures.OAK ||
+               tree == TreeFeatures.OAK_BEES_002 ||
+               tree == TreeFeatures.OAK_BEES_005 ||
+               tree == TreeFeatures.FANCY_OAK ||
+               tree == TreeFeatures.FANCY_OAK_BEES_002 ||
+               tree == TreeFeatures.FANCY_OAK_BEES_005 ||
+               tree == TreeFeatures.FANCY_OAK_BEES;
+    }
 }
